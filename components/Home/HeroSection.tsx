fe84aa4604210e82ee98c5c1eeda326f9c8f5b67
jyotijaiinfoway <jyoti.kumari@jaiinfoway.com>
2023-11-16 17:59:51 +0530

    signup  signin verifyemail ui added

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -4,6 +4,7 @@ import Philosophy from "../../app/images/philosphy.png";
 import Community from "../../app/images/community.png";
 import SawamiJi from "../../app/images/sawamiji.png";
 import Image from 'next/image';
+import { BGCOLOR } from '../icons/bgColor';
 
 const HeroSection = () => {
     const [selectedImage, setSelectedImage] = useState(2);
@@ -69,14 +70,14 @@ const HeroSection = () => {
 
 
             </div>
-            <div className='flex flex-col justify-center items-center text-center w-full h-[436px]  border border-gray-200 '>
-                
-                 <div className='  flex flex-col items-center   flex-shrink-0 gap-8 bg-gradient-to-r from-transparent via-opacity-10 to-opacity-0 via-opacity-90 to-white text-[#291900]'>
+            <div className='flex relative flex-col justify-center items-center text-center w-full h-[436px]  '>
+                    <BGCOLOR  className='h-full w-full absolute top-0 -left-32 '/>
+                    <div className='  flex flex-col items-center   flex-shrink-0 gap-8 bg-gradient-to-r from-transparent via-opacity-10 to-opacity-0 via-opacity-90 to-white text-[#291900]'>
                     
                     <h2 className='text-center font-semibold leading-6 text-[40px] '>Jagadguru Kripaluji Yog</h2>
-                    <p className='self-stretch text-[#645743] text-center font-SatoshiVariable text-[18px] font-normal break-words' >JKYog (Jagadguru Kripaluji Yog) is a non-profit charitable organization established for the physical, mental, and spiritual well-being of all humankind. It disseminates authentic knowledge of Yoga for the body, mind, and soul. It serves the needy, promotes education for the rural youth and provides healthcare for the underprivileged.</p>
-                    <button className='bg-[#EF6F1F]  font-semibold mt-2 flex justify-center border-[#dad3c8] text-lg gap-[2px] rounded-3xl px-[32px] py-[14px]  text-white  '>About us</button>
-
+                    <p className='self-stretch text-[#645743] text-center font-SatoshiVariable lg:w-[1020px] w-auto text-[18px] font-normal break-words' >JKYog (Jagadguru Kripaluji Yog) is a non-profit charitable organization established for the physical, mental, and spiritual well-being of all humankind. It disseminates authentic knowledge of Yoga for the body, mind, and soul. It serves the needy, promotes education for the rural youth and provides healthcare for the underprivileged.</p>
+                     <button className='bg-[#EF6F1F]  font-semibold mt-2 flex justify-center border-[#dad3c8] text-lg gap-[2px] rounded-3xl px-[32px] py-[14px]  text-white  '>About us</button>
+                    <BGCOLOR  className='h-full w-full absolute top-0 -right-32'/>
                 </div>
             </div>
            
