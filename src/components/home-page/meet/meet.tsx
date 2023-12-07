ab74326a5bbc0956876dd75cf5539af5c961269c
oleeh-shd <oleehshd@gmail.com>
2023-12-07 15:52:16 +0200

    meet section mobile

--- src/components/home-page/meet/meet.tsx
+++ src/components/home-page/meet/meet.tsx
@@ -4,9 +4,11 @@ import { Places } from './places';
 
 const Meet: FC = () => {
   return (
-    <section className="relative flex items-center justify-center bg-[#edf3e5] w-full flex-col  gap-10 self-stretch pt-[100px] pb-[120px] px-10 rounded-[40px] bg-meet-section-bg">
-      <h2 className="text-heading text-center text-[44px] font-ptserif font-bold leading-[56px]">Meet Swamiji</h2>
-      <div className="flex w-[1360px] justify-end items-center gap-5 ">
+    <section className="relative flex items-center justify-center bg-[#edf3e5] w-full flex-col gap-6 lg:gap-10 py-8 lg:pt-[100px] lg:pb-[120px] px-4 lg:px-10 rounded-[40px] bg-meet-section-bg">
+      <h2 className="text-heading text-center text-[32px] lg:text-[44px] font-ptserif font-bold leading-10 lg:leading-[56px]">
+        Meet Swamiji
+      </h2>
+      <div className="flex flex-col xl:flex-row justify-end items-center gap-5">
         <Poster />
         <Places />
       </div>
--- src/components/home-page/meet/poster.tsx
+++ src/components/home-page/meet/poster.tsx
@@ -14,28 +14,26 @@ export const Poster: FC = () => {
   return data.length
     ? data.map(({ attributes: { poster, place, date, heading, venue } }, i) => {
         return (
-          <div key={i}>
-            <div className="flex w-[785px] flex-col justify-center items-start shrink-0 p-2 rounded-[20px] bg-white">
-              <Image
-                src={poster.data[0].attributes.url}
-                alt="poster-image"
-                width={769}
-                height={410}
-                className="rounded-2xl"
-              />
-              <div className="flex items-end gap-2.5 self-stretch p-4">
-                <div className="flex flex-col justify-center items-start gap-1 flex-[1_0_0]">
-                  <div className="flex items-center gap-2 self-stretch">
-                    <Badge src="/icons/location.png" text={place} />
-                    <Badge src="/icons/calendar.svg" text={formatDate(date)} />
-                  </div>
-                  <h3 className="text-heading text-[32px] font-ptserif font-bold leading-10">{heading}</h3>
-                  <span className="text-primary text-base font-satoshi-regular font-normal leading-6 tracking-[-0.16px]">
-                    {venue}
-                  </span>
+          <div key={i} className="flex flex-col justify-center items-start shrink-0 p-2 rounded-[20px] bg-white">
+            <Image
+              src={poster.data[0].attributes.url}
+              alt="poster-image"
+              width={769}
+              height={410}
+              className="rounded-2xl"
+            />
+            <div className="flex items-end gap-2.5 self-stretch p-4">
+              <div className="flex flex-col justify-center items-start gap-1 flex-[1_0_0]">
+                <div className="flex items-center gap-2 self-stretch">
+                  <Badge src="/icons/location.png" text={place} />
+                  <Badge src="/icons/calendar.svg" text={formatDate(date)} />
                 </div>
-                <Arrow direction="topLeft" />
+                <h3 className="text-heading text-2xl lg:text-[32px] font-ptserif font-bold lg:leading-10">{heading}</h3>
+                <span className="text-primary text-base font-satoshi-regular font-normal leading-6 tracking-[-0.16px]">
+                  {venue}
+                </span>
               </div>
+              <Arrow direction="topLeft" />
             </div>
           </div>
         );
